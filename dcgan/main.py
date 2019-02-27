fb2f59eaf505064ea727da5dc86a5849214fe998
Kathryn Webb <kathryn.webb@akqa.com>
2019-02-27 12:18:06 +0000

    tweaked train size as infinity was a float

--- dcgan/main.py
+++ dcgan/main.py
@@ -10,7 +10,7 @@ flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epochs to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
-flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+flags.DEFINE_integer("train_size", 10000, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
 flags.DEFINE_integer("input_height", None, "The size of image to use (will be center cropped). [108]")
 flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
